7c0be26539a4c42f3bcc407bd76770144604b3db
Alendroide <alendroideyt@gmail.com>
2025-05-05 08:44:33 -0500

    Filename logic updated in updateAB controller

--- src/controllers/pet.controller.js
+++ src/controllers/pet.controller.js
@@ -95,12 +95,15 @@ async function updateAB(req,res){
         if(!petAB) return res.status(404).json({status : 404, msg : 'Pet not found'});
         
         const { name, race_id, gender_id } = req.body;
-        const fileAB = req.file.filename || petAB.photo;
+        const filenameAB = req.file?.filename;
+        let fileAB;
+        if(!filenameAB) fileAB = petAB.photo;
+        else fileAB = `pets/${filenameAB}`;
 
         const updatedPetAB = await prismaAB.pet.update({
             where : { id },
             data : {
-                photo : `pets/${fileAB}`,
+                photo : `${fileAB}`,
                 name : name || petAB.name,
                 race_id : parseInt(race_id) || petAB.race_id,
                 gender_id : parseInt(gender_id) || petAB.gender_id
